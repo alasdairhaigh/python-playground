c9175abba87d51b6ce2960fe683c10ad081a15cd
Alasdair Haigh <alasdair.haigh@gmail.com>
2018-07-30 22:59:19 +0100

    Basic operators the way they wanted me to do it

--- tutorial4.py
+++ tutorial4.py
@@ -2,8 +2,8 @@ x = object()
 y = object()
 
 # TODO: change this code
-x_list = [x,x,x,x,x,x,x,x,x,x]
-y_list = [y,y,y,y,y,y,y,y,y,y]
+x_list = [x] * 10
+y_list = [y] * 10
 big_list = x_list + y_list
 
 print("x_list contains %d objects" % len(x_list))
